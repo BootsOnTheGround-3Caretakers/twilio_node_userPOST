0c95ce5c06957a7c8d4e15d5765efbd169898890
dylan1218@live.com <dylan1218@live.com>
2020-03-31 18:33:55 -0400

    capture conversation responses

--- index.js
+++ index.js
@@ -17,41 +17,74 @@ app.use(session({ secret: 'anything-you-want-but-keep-secret' }));
 app.post('/sms', (req, res) => {
     const smsCount = req.session.counter || 0;
     const responseOne = req.session.response1 || "null"
-    const responseTwo = req.session.response2 || "null"
-    var body = req.body.Body
+    const responseTwoA = req.session.response2A || "null" //not currently utilized
+    const responseTwoB = req.session.response2B || "null" //
     const twiml = new MessagingResponse();
+    var body = req.body.Body
+    body = body.toString();
 
+    console.log(body);
+    console.log(smsCount);
     //message handler
     //initial response
     if (smsCount == 0) {
-        message = 'Hi! Thanks for reaching out to 3Hands. To get started respond with 1 if you are looking for help, 2 if you are providing help and 3 if both.';
+        console.log("if one called");
+        if ((body === "1" || body === "2" || body === "3")) {
+            console.log("if one, part two called");
+            req.session.response1 = body
+            req.session.counter = smsCount + 1;
+            var currentSessionsmsCount = smsCount + 1;
+            var currentSessionResponse1 = body
+            console.log(currentSessionResponse1)
+        } else {
+            message = 'Hi! Thanks for reaching out to 3Hands. To get started respond with 1 if you are looking for help, 2 if you are providing help and 3 if both.';
+            /*
+            if (body == undefined) {
+                message = "Your response was something other than 1, 2 or 3. Please reply back with one of the three options."
+            }
+            */
+        };
     }
     //Note: to add an error response if not equal to 1, 2 or 3 for initial response
 
     //second response if correct
-    if ((smsCount == 1) && (responseOne == 1)) {
-        message = "Great, you're looking to help. Briefly describe how you're looking to help"
-    }
+
+    if ((currentSessionsmsCount === 1 || smsCount === 1)) {
+        console.log("if two called")
+        if (currentSessionResponse1 === "1") {
+            console.log("if two, part one called")
+            message = "Great, you're looking to help. Briefly describe how you're looking to help (grocery delivery, a friendly call, etc)."
+                //req.session.response2A = body
+                //req.session.response2B = ""
+            jsonResponseObject = { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": "", "careDesciprtion": body }
+            req.session.counter = smsCount + 1;
+            var currentSessionsmsCount = smsCount + 1;
+        }
+        if (currentSessionResponse1 === "2") {
+            message = "Great, you're looking for help. Briefly describe what kind of help you're looking for (grocery delivery, a friendly call, etc). "
+            req.session.response2A = ""
+            req.session.response2B = body
+            jsonResponseObject = { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": body, "careDesciprtion": "" }
+            req.session.counter = smsCount + 1;
+            var currentSessionsmsCount = smsCount + 1;
+        };
+        if (currentSessionResponse1 === "3") {
+            message = "Great, you're looking for help, AND providing help. First, briefly describe how you're looking to help"
+            req.session.response2A = body
+            if (responseTwoB = "") {
+                req.session.response2B = ""
+            }
+        };
+    };
+
 
     //final response
-    if ((smsCount == 2)) {
+    if ((currentSessionsmsCount == 2)) {
         message = "Thanks for providing your information! We'll get back to you in 48-72 hours."
-        var responseVar =
-            handleResponses.postUserResponse()
-    }
 
-    //session variable handler, the below is utilized to track
-    //what step in the conversation we are, and record
-    //responses.
-    if ((smsCount == 0) && (body = (1 || 2 || 3))) { //if first message response, and correct response of 1, 2 or 3
-        req.session.response1 = body
-        req.session.counter = smsCount + 1;
+        console.log(jsonResponseObject)
+        var responseVar = handleResponses.postUserResponse()
     }
-    if ((smsCount == 1)) {
-        req.session.response2 = body
-        req.session.counter = smsCount + 1;
-    }
-
 
     twiml.message(message);
     res.writeHead(200, { 'Content-Type': 'text/xml' });
