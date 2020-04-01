3e55f9cb99b84b2a5c64cf889e966d6db657d89c
dylan1218@live.com <dylan1218@live.com>
2020-03-31 22:08:31 -0400

    finish conversation logic and POST request to TwilioBlob API

--- httpRequests.js
+++ httpRequests.js
@@ -1,13 +1,28 @@
-function postUserResponse() {
-    console.log("post response called")
-        //to add POST logic here
-    return "Hello";
+var request = require('request');
+
+function postUserResponse(JSONObject) {
+    // Build the post string from an object
+    //var post_data = JSON.stringify(JSONObject)
+    //var post_data = JSONObject
+
+    var myJSONObject = JSONObject;
+
+    request({
+        url: "http://c29a9953.ngrok.io/api/v1/TwilioBlob",
+        method: "POST",
+        json: true, // <--Very important!!!
+        headers: {
+            "Authorization": "3CAREGIVERS",
+            "content-Type": "application/json"
+        },
+        body: myJSONObject
+    }, function(error, response, body) {
+        console.log(response);
+    });
+
 }
 
 
 module.exports = {
-    postUserResponse: postUserResponse,
-    bar: function() {
-        console.log("bar called")
-    }
+    postUserResponse: postUserResponse
 };
--- index.js
+++ index.js
@@ -15,11 +15,20 @@ app.get('/', (req, res) => {
 app.use(session({ secret: 'anything-you-want-but-keep-secret' }));
 
 app.post('/sms', (req, res) => {
+    //These variables represent responses, or helper variables to obtain responses.
+    //All variables are set to session objects due to the stateless nature of the
+    //sms sendings/receipts. The session objects take advantage of twilio's cookie
+    //features which helps to track conversations under different numbers.
     const smsCount = req.session.counter || 0;
-    const responseOne = req.session.response1 || "null"
-    const responseTwoA = req.session.response2A || "null" //not currently utilized
+    const responseOne = req.session.response1 || "null" //responseOne needs a helper function to translate 1/2/3 options into a better "response" before placing in JSON object. Currently 1 represents needer, 2 helper, 3 both.
+    const responseTwoA = req.session.response2A || "null" //
+    const responseTwoAHelper = req.session.response2AHelper || "null"
     const responseTwoB = req.session.response2B || "null" //
+    const responseTwoBHelper = req.session.response2BHelper || "null"
+
     const twiml = new MessagingResponse();
+
+    //represents the request response (i.e. the users text)
     var body = req.body.Body
     body = body.toString();
 
@@ -28,9 +37,7 @@ app.post('/sms', (req, res) => {
     //message handler
     //initial response
     if (smsCount == 0) {
-        console.log("if one called");
         if ((body === "1" || body === "2" || body === "3")) {
-            console.log("if one, part two called");
             req.session.response1 = body
             req.session.counter = smsCount + 1;
             var currentSessionsmsCount = smsCount + 1;
@@ -38,41 +45,42 @@ app.post('/sms', (req, res) => {
             console.log(currentSessionResponse1)
         } else {
             message = 'Hi! Thanks for reaching out to 3Hands. To get started respond with 1 if you are looking for help, 2 if you are providing help and 3 if both.';
-            /*
-            if (body == undefined) {
-                message = "Your response was something other than 1, 2 or 3. Please reply back with one of the three options."
-            }
-            */
         };
     }
-    //Note: to add an error response if not equal to 1, 2 or 3 for initial response
 
-    //second response if correct
-
+    //second response handler
     if ((currentSessionsmsCount === 1 || smsCount === 1)) {
         console.log("if two called")
-        if (currentSessionResponse1 === "1") {
+        if (currentSessionResponse1 === "1" || responseOne === "1") {
             console.log("if two, part one called")
             message = "Great, you're looking to help. Briefly describe how you're looking to help (grocery delivery, a friendly call, etc)."
                 //req.session.response2A = body
                 //req.session.response2B = ""
-            jsonResponseObject = { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": "", "careDesciprtion": body }
+            jsonResponseObject = { "TwilioJSONBlob": { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": body, "careDesciprtion": responseTwoA } }
             req.session.counter = smsCount + 1;
             var currentSessionsmsCount = smsCount + 1;
         }
-        if (currentSessionResponse1 === "2") {
+        if (currentSessionResponse1 === "2" || responseOne === "2") {
             message = "Great, you're looking for help. Briefly describe what kind of help you're looking for (grocery delivery, a friendly call, etc). "
             req.session.response2A = ""
             req.session.response2B = body
-            jsonResponseObject = { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": body, "careDesciprtion": "" }
+            jsonResponseObject = { "TwilioJSONBlob": { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": body, "careDesciprtion": responseTwoA } }
             req.session.counter = smsCount + 1;
             var currentSessionsmsCount = smsCount + 1;
         };
-        if (currentSessionResponse1 === "3") {
+        if (currentSessionResponse1 === "3" || responseOne === "3") {
             message = "Great, you're looking for help, AND providing help. First, briefly describe how you're looking to help"
-            req.session.response2A = body
-            if (responseTwoB = "") {
-                req.session.response2B = ""
+            req.session.response2AHelper = "set" //keeps stack of the step in the conversation
+            if (responseTwoAHelper == "set") {
+                message = "Now briefly describe what help you're looking for"
+                req.session.response2A = body
+                req.session.response2BHelper = "set" //keeps stack of the step in the conversation
+            }
+            if (responseTwoBHelper == "set") {
+                req.session.response2B = body
+                jsonResponseObject = { "TwilioJSONBlob": { "phoneNumber": 123, "needer": responseOne, "caretaker": responseOne, "needDescription": body, "careDesciprtion": responseTwoA } }
+                req.session.counter = smsCount + 1;
+                var currentSessionsmsCount = smsCount + 1;
             }
         };
     };
